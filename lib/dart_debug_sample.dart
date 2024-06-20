3d38814c1a6965bbce740f19eb53edb0e6e98400
S-YUN <https://github.com/S-YUN>
2024-06-20 14:03:54 +0900

    feedback 후 리팩

--- lib/dart_debug_sample.dart
+++ lib/dart_debug_sample.dart
@@ -1,25 +1,14 @@
-import 'package:intl/intl.dart';
-
 class YukymController {
-  String nowDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
-
   // 1. 자시의 국 : 갑자1국 = getTyOne()의 값
   // month를 바탕으로 인덱스 계산
   String getTyA() {
-    int month = int.parse(nowDate.substring(5, 7));
-
-    if (month < 1 || month > 12) {
-      return '경오7국';
-    }
-    String index = ((month + 1) ~/ 2).toString();
-    return '경오$index국';
+    int month = DateTime.now().month;
+    return '경오${((month + 1) ~/ 2).toString()}국';
   }
 
+  // 2시간 단위로 인덱스 계산
   String getTyB() {
-    // 2시간 단위로 인덱스 계산
     int nowHour = DateTime.now().hour;
-    String index = ((nowHour ~/ 2) + 1).toString();
-
-    return '갑자$index국';
+    return '갑자${((nowHour ~/ 2) + 1).toString()}국';
   }
 }
