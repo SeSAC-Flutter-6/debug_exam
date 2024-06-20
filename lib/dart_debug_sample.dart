deb601c36b5648b026e3f79720d6ebfb291954cf
S-YUN <https://github.com/S-YUN>
2024-06-20 11:03:03 +0900

    1. getTyA()는 월 값을 기준으로 계산, getTyB()는 시간을 기준으로 계산한다고 추정 2. YukymTimeModel이 주어진 코드만 봤을때는 어떤 의미를 가지는지 알 수 없어서 삭제해버림.. 3. getTyA()에서 월 값은 1~12 외의 다른 값이 올 때는 경오 7국 으로 반환하는 월 유효성검사. 4. getTyB()에서는 시간 값이 항상 0~24를 보장하므로 유효성 검사 필요없다고 판단. 5. 사실 이렇게 맘대로 바꿔도 되는진 모르겠지만 그냥 해봄

--- lib/dart_debug_sample.dart
+++ lib/dart_debug_sample.dart
@@ -1,81 +1,25 @@
 import 'package:intl/intl.dart';
 
 class YukymController {
-  // DateTime.parse(_userData.value!.selectDate)
   String nowDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
 
-  late String nowTime;
-
   // 1. 자시의 국 : 갑자1국 = getTyOne()의 값
-String getTyA() {
-  List<YukymTimeModel> timeDataOne = _getTimeDataOne(nowDate);
-  int month = int.parse(nowDate.substring(5, 7));
+  // month를 바탕으로 인덱스 계산
+  String getTyA() {
+    int month = int.parse(nowDate.substring(5, 7));
 
-  if (timeDataOne.isEmpty) {
-    return '경오7국';
+    if (month < 1 || month > 12) {
+      return '경오7국';
+    }
+    String index = ((month + 1) ~/ 2).toString();
+    return '경오$index국';
   }
-  if (month < 1 || month > 12) {
-    return timeDataOne.first.ty1; 
-  }
-  //month값이 유효하면 index계산
-  String index = ((month + 1) ~/ 2).toString();
-  return '경오$index국';
-}
 
   String getTyB() {
-    List<YukymTimeModel> timeDataOne = _getTimeDataOne(nowDate);
-    String result = timeDataOne.first.ty12;
+    // 2시간 단위로 인덱스 계산
+    int nowHour = DateTime.now().hour;
+    String index = ((nowHour ~/ 2) + 1).toString();
 
-    final nowTime = DateTime.now();
-    if (nowTime.hour >= 0 && nowTime.hour < 2) {
-      return timeDataOne.first.ty1;
-    } else if (nowTime.hour >= 2 && nowTime.hour < 4) {
-      return timeDataOne.first.ty2;
-    } else if (nowTime.hour >= 4 && nowTime.hour < 6) {
-      return timeDataOne.first.ty3;
-    } else if (nowTime.hour >= 6 && nowTime.hour < 8) {
-      return timeDataOne.first.ty4;
-    } else if (nowTime.hour >= 8 && nowTime.hour < 10) {
-      return timeDataOne.first.ty5;
-    } else if (nowTime.hour >= 10 && nowTime.hour < 12) {
-      return timeDataOne.first.ty6;
-    } else if (nowTime.hour >= 12 && nowTime.hour < 14) {
-      return timeDataOne.first.ty7;
-    } else if (nowTime.hour >= 14 && nowTime.hour < 16) {
-      return timeDataOne.first.ty8;
-    } else if (nowTime.hour >= 16 && nowTime.hour < 18) {
-      return timeDataOne.first.ty9;
-    } else if (nowTime.hour >= 18 && nowTime.hour < 20) {
-      return timeDataOne.first.ty10;
-    } else if (nowTime.hour >= 20 && nowTime.hour < 22) {
-      return timeDataOne.first.ty11;
-    } else if (nowTime.hour >= 22 && nowTime.hour < 24) {
-      return timeDataOne.first.ty12;
-    }
-
-    return result;
+    return '갑자$index국';
   }
-
-  List<YukymTimeModel> _getTimeDataOne(String nowDate) {
-    List<YukymTimeModel> timeDataOne = [];
-    for (int i = 0; i < 24; i++) {
-      timeDataOne.add(YukymTimeModel());
-    }
-    return timeDataOne;
-  }
-}
-
-class YukymTimeModel {
-  String ty1 = '갑자1국';
-  String ty2 = '갑자2국';
-  String ty3 = '갑자3국';
-  String ty4 = '갑자4국';
-  String ty5 = '갑자5국';
-  String ty6 = '갑자6국';
-  String ty7 = '갑자7국';
-  String ty8 = '갑자8국';
-  String ty9 = '갑자9국';
-  String ty10 = '갑자10국';
-  String ty11 = '갑자11국';
-  String ty12 = '갑자12국';
 }
